5beab83c298a1017b29f1bde920ae302b18082ee
aBugInTheSystem <ekom338.com@gmail.com>
2023-09-27 15:04:10 +0300

    Gallery done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,23 @@
 /* eslint-disable react/prop-types */
-export default function Card({ImageData}) {
+export default function Card({ ImageData }) {
   return (
-    <div className="">
-      <img src={ImageData.url} alt="" className="rounded-md hover:blur-sm transition-all" />
+    <div className="font-Poppins relative">
+      {ImageData.type == "image" ? (
+        <img
+          src={ImageData.url}
+          alt=""
+          className="rounded-md hover:blur-sm transition-all h-auto w-auto"
+        />
+      ) : (
+        <video src={ImageData.videoUrl} autoPlay muted></video>
+          
+      )}
+
+      <div>
+        <h1 className="absolute top-3 left-2 text-white font-[300]">
+          {ImageData.title}
+        </h1>
+      </div>
     </div>
   );
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ export default function Gallery() {
   return (
     <div className="flex justify-center items-center mt-36 ">
       <div className="mt-52">
-        <div className="grid gap-4 grid-cols-2 mx-44">
+        <div className="grid gap-7 grid-cols-2 mx-44">
          {ImageData.map((image)=>(
           <Card ImageData={image} key={image.id}/>
          ))}
