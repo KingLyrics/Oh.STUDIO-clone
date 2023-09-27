47cb4e50feabc9d1b8802d82774c1bd3acebcba8
aBugInTheSystem <ekom338.com@gmail.com>
2023-09-27 11:02:02 +0300

    Intial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function Navbar() {
+  const [activeLink, setactiveLink] = useState("Home");
+
+  const handleClick = (Link) => {
+    setactiveLink(Link);
+  };
+
+  return (
+    <nav className="max-w-[300px] mx-auto text-center fixed top-7 left-0 right-0 bg-SlateGray space-x-7 rounded-full py-3 font-Roboto font-[400] text-sm">
+      <a
+        href=""
+        className={
+          activeLink === "Home" ? "bg-white rounded-full py-3 px-6 -ml-7" : ""
+        }
+        onClick={() => handleClick("Home")}
+      >
+        Home
+      </a>
+      <a
+        href="#profile"
+        className={
+          activeLink === "Profile"
+            ? "bg-white rounded-full py-3 px-6 -ml-7"
+            : ""
+        }
+        onClick={() => handleClick("Profile")}
+      >
+        Profile
+      </a>
+      <a
+        href="#contact"
+        className={
+          activeLink === "Contact"
+            ? "bg-white rounded-full py-3 px-6 -mr-7"
+            : ""
+        }
+        onClick={() => handleClick("Contact")}
+      >
+        Contact
+      </a>
+    </nav>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        SlateGray: "#e8e5e480",
+        OffGray: "#7f7f7f",
+      },
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
